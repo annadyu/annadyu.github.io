acc71e4192b8b35366492f1e93c0ccc982ebcfca
dyuanna <annadyu666@gmail.com>
2025-11-29 02:12:47 +0900

    commit

--- src/layouts/PrivateRoute.jsx
+++ src/layouts/PrivateRoute.jsx
@@ -2,7 +2,7 @@ import { Outlet, Navigate } from "react-router-dom";
 
 const PrivateRoute = () => {
   const savedUser = JSON.parse(localStorage.getItem("registeredUser"));
-  return savedUser ? <Outlet /> : <Navigate to="/login/sign-in" replace />;
+  return savedUser ? <Outlet /> : <Navigate to="/sign-in" replace />;
 };
 
 export default PrivateRoute;
